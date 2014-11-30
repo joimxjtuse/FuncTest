72f16706f435c2e3d4aba3487045ae88bf1dfc2f
joim <joimxjtuse@gmail.com>
2014-11-30 17:29:42 +0800

    init some doc

--- src/cn/joim/test/Hello.java
+++ src/cn/joim/test/Hello.java
@@ -2,6 +2,13 @@ package cn.joim.test;
 
 public class Hello {
 
+	/**
+	 * this pro is used for record some algorithm what I see during work and
+	 * life.
+	 * 
+	 * 2014.11.30 the first commit.
+	 * 
+	 * */
 	public static void main(String[] args) {
 		System.out.println("Hello,World.");
 
