340592ed008d44b84afab18b3cf8620350673580
joim <joimxjtuse@gmail.com>
2014-11-30 17:55:29 +0800

    interception an user's nick name. if user's name is too longer, get 8 Chinese characters. here, an Chinese characters equals an character, an English equals  Half of the characters.

---
+++ src/cn/joim/string/sub/TestSubString.java
@@ -0,0 +1,27 @@
+package cn.joim.string.sub;
+
+public class TestSubString {
+
+	/**
+	 * this pro is used for record some algorithm what I see during work and
+	 * life.
+	 * 
+	 * 2014.11.30 the first commit.
+	 * 
+	 * */
+	public static void main(String[] args) {
+
+		SubStringMyNickName mNickNameSub = new SubStringMyNickName();
+
+		for (int i = 0, length = nameArray.length; i < length; i++) {
+
+			System.out.println("src=" + nameArray[i] + " ;result="
+					+ mNickNameSub.subMyNickName(nameArray[i]));
+		}
+
+	}
+
+	private static String nameArray[] = { "joim你好，大苏打丰富的斯蒂芬", "发生的房贷首付第三方的首付第",
+			"fsdfsfsdfdsfdsdfdsfdsf", "打算fdfd发生g的房首付第三方岁" };
+
+}
