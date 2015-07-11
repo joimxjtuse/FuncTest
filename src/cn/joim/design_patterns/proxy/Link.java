5e851b596c0a651569149bf65e97d411056c95f2
joim <joimxjtuse@gmail.com>
2015-07-11 15:53:32 +0800

    <设计模式沉思录> 代理模式整理， 实现类似于快捷方式的功能。

---
+++ src/cn/joim/design_patterns/proxy/Link.java
@@ -0,0 +1,28 @@
+package cn.joim.design_patterns.proxy;
+
+import cn.joim.design_patterns.component.Node;
+
+public class Link implements Node {
+
+	/**
+	 * 
+	 * Proxy：用来维护一个引用，并让该代理去访问实际对象。 </br> so, mProxyNode = File/Directory.
+	 * 
+	 * */
+	Node mProxyNode;
+
+	public Link(Node mProxy) {
+		this.mProxyNode = mProxy;
+	}
+
+	@Override
+	public int size() {
+		return mProxyNode.size();
+	}
+
+	@Override
+	public Node get(int n) {
+		return mProxyNode.get(n);
+	}
+
+}
