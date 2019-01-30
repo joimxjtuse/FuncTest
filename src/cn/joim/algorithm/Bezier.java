bcfbd35a20cb7baabb5dd503749cf84319adc7f6
joim <747604141@qq.com>
2019-01-30 14:38:02 +0800

    import project with idea.

--- src/cn/joim/algorithm/Bezier.java
+++ src/cn/joim/algorithm/Bezier.java
@@ -1,9 +1,6 @@
 package cn.joim.algorithm;
 
 /**
- * 
- * 
- * 
  * B(t) = C(n,i) Pi (1 - t)^(n - i) (t)^(i), t = [0, 1];
  * <p>
  * n是阶数，一阶的时候是线性方程，阶数是 确定点的个数- 1.
@@ -14,69 +11,69 @@ package cn.joim.algorithm;
  * <p>
  * t表示时间变量，并且介于0， 1 之间，可以等于0 和1;
  * <p>
- * */
+ */
 public class Bezier {
 
-	private static class Point {
+    private static class Point {
 
-		double x;
-		double y;
-	}
+        double x;
+        double y;
+    }
 
-	private static void bezier(Point[] pointArrs, double t) {
+    private static void bezier(Point[] pointArrs, double t) {
 
-		int i, n = pointArrs.length - 1;
-		double x = 0.0, y = 0.0;
-		for (i = 0; i < n; i++) {
-			x += fn(pointArrs[i].x, n, i, t);
+        int i, n = pointArrs.length - 1;
+        double x = 0.0, y = 0.0;
+        for (i = 0; i < n; i++) {
+            x += fn(pointArrs[i].x, n, i, t);
 
-			y += fn(pointArrs[i].y, n, i, t);
+            y += fn(pointArrs[i].y, n, i, t);
 
-		}
-		System.out.println(" x = " + x + " ; y = " + y);
+        }
+        System.out.println(" x = " + x + " ; y = " + y);
 
-	}
+    }
 
-	private static double fn(double p, int n, int i, double t) {
+    private static double fn(double p, int n, int i, double t) {
 
-		return arrangement(n, i) * p * Math.pow(1 - t, n - i) * Math.pow(t, i);
+        return arrangement(n, i) * p * Math.pow(1 - t, n - i) * Math.pow(t, i);
 
-	}
+    }
 
-	private static int arrangement(int n, int i) {
+    private static int arrangement(int n, int i) {
 
-		return factorial(n) / (factorial(n - i) * factorial(i));
-	}
+        return factorial(n) / (factorial(n - i) * factorial(i));
+    }
 
-	private static int factorial(int n) {
-		if (n >= Integer.MAX_VALUE || n <= 0) {
-			return 1;
-		}
-		int s = 1;
-		while (n != 0) {
-			s *= n--;
-		}
-		return s;
-	}
+    private static int factorial(int n) {
+        if (n >= Integer.MAX_VALUE || n <= 0) {
+            return 1;
+        }
+        int s = 1;
+        while (n != 0) {
+            s *= n--;
+        }
+        return s;
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		Point[] pointArrs = new Point[10];
-		Point tmpPoint = null;
-		for (int i = 0; i < pointArrs.length; i++) {
+        Point[] pointArrs = new Point[10];
+        Point tmpPoint = null;
+        for (int i = 0; i < pointArrs.length; i++) {
 
-			tmpPoint = new Point();
-			tmpPoint.x = 1 + (Math.random() * 100);
-			tmpPoint.y = 1 + (Math.random() * 100);
-			pointArrs[i] = tmpPoint;
-		}
-		for (double t = 0; t < 1.0; t += 0.001) {
+            tmpPoint = new Point();
+            tmpPoint.x = 1 + (Math.random() * 100);
+            tmpPoint.y = 1 + (Math.random() * 100);
+            pointArrs[i] = tmpPoint;
+        }
+        for (double t = 0; t < 1.0; t += 0.001) {
 
-			bezier(pointArrs, t);
-		}
-	}
+            bezier(pointArrs, t);
+        }
+    }
 
 }
--- src/cn/joim/string/sub/SubStringMyNickName.java
+++ src/cn/joim/string/sub/SubStringMyNickName.java
@@ -1,62 +1,61 @@
 package cn.joim.string.sub;
 
 /**
- * 
  * 截取8个字符的中文昵称， 其英文算半个字符,中文算1个字符。 </br>
- * 
+ * <p>
  * 输入：“joim你好，大苏打丰富的斯蒂芬” 输出：“joim你好，大苏打” 10个字符</br> 输入：“发生的房贷首付第三方的首付第”
  * 输出：“发生的房贷首付第” 8个中文字符. </br> 输入：“fsdfsfsdfdsfdsdfdsfdsf” 输出：“fsdfsfsdfdsfdsdf”
  * 16个英文字符。 </br> 输入：“打算fdfd发生g的房首付第三方岁” 输出：“打算fdfd发生g的房” 8.5</br> 1--1/2</br>
- * */
+ */
 public class SubStringMyNickName {
 
-	private static final int SUBSTRING_LENGTH = 8;
+    private static final int SUBSTRING_LENGTH = 8;
 
-	public String subMyNickName(String mName) {
+    public String subMyNickName(String mName) {
 
-		return subStrigByLanguage(mName, SUBSTRING_LENGTH);
-	}
+        return subStrigByLanguage(mName, SUBSTRING_LENGTH);
+    }
 
-	private String subStrigByLanguage(String res, int length) {
-		if (0 == length) {
-			return "";
-		}
-		if (isEmpty(res) || res.length() <= length) {
-			return res;
-		}
-		StringBuilder mStringBuilder = new StringBuilder();
-		int resultLength = 0;
-		for (int i = 0, mResLength = res.length(); i < mResLength
-				&& resultLength < 2 * length; i++) {
+    private String subStrigByLanguage(String res, int length) {
+        if (0 == length) {
+            return "";
+        }
+        if (isEmpty(res) || res.length() <= length) {
+            return res;
+        }
+        StringBuilder mStringBuilder = new StringBuilder();
+        int resultLength = 0;
+        for (int i = 0, mResLength = res.length(); i < mResLength
+                && resultLength < 2 * length; i++) {
 
-			char c = res.charAt(i);
-			if (!isLetter(c)) {
-				// 非英文字符.
-				resultLength += 2;
-			} else {
-				resultLength += 1;
-			}
-			mStringBuilder.append(c);
-		}
-		return mStringBuilder.toString();
-	}
+            char c = res.charAt(i);
+            if (!isLetter(c)) {
+                // 非英文字符.
+                resultLength += 2;
+            } else {
+                resultLength += 1;
+            }
+            mStringBuilder.append(c);
+        }
+        return mStringBuilder.toString();
+    }
 
-	/**
-	 * 检测是汉字还是英文或数字
-	 * 
-	 * @param c
-	 * @return
-	 */
-	private boolean isLetter(char c) {
-		int k = 0x80;
-		return c / k == 0 ? true : false;
-	}
+    /**
+     * 检测是汉字还是英文或数字
+     *
+     * @param c
+     * @return
+     */
+    private boolean isLetter(char c) {
+        int k = 0x80;
+        return c / k == 0 ? true : false;
+    }
 
-	private boolean isEmpty(String res) {
-		if (null == res || 0 == res.length())
-			return true;
-		else
-			return false;
-	}
+    private boolean isEmpty(String res) {
+        if (null == res || 0 == res.length())
+            return true;
+        else
+            return false;
+    }
 
 }
