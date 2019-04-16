7936760f78be2340d4608b354c1bf6a483af00e5
joim <joimxjtuse@gmail.com>
2019-04-16 09:32:50 +0800

    commit tree node class.

---
+++ src/cn/joim/algorithm/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package cn.joim.algorithm.tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+
+    public static TreeNode createTreeFrom(int arrays[]) {
+        TreeNode head = null;
+
+        return head;
+    }
+}
