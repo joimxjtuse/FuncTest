c4be0fbba1a5346ba301d3d772a6aec3c9e54d1e
joim <joimxjtuse@gmail.com>
2019-05-12 12:07:17 +0800

    加解密测试.

--- src/cn/joim/jdk8/Use3DES.java
+++ src/cn/joim/jdk8/Use3DES.java
@@ -30,4 +30,35 @@ public class Use3DES {
         }
         return value;
     }
+
+    public static byte[] decrypt(byte[] key, byte[] src) {
+        byte value[] = null;
+        try {
+            //秘钥key.
+            SecretKey desKey = new SecretKeySpec(key, ALGORITHM);
+            //执行解密操作.
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, desKey);
+            value = cipher.doFinal(src);
+        } catch (Exception exce) {
+            exce.printStackTrace();
+        }
+        return value;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            byte key[] = "01234567899876543210abcd".getBytes();
+            byte[] src = "测试数据".getBytes("UTF-8");
+
+            byte encoded[] = encrpt(key, src);
+
+            byte decodede[] = decrypt(key, encoded);
+            System.out.println("加密后数据: " + new String(encoded) + " ; 解密后数据: " + new String(decodede, "UTF-8"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
